aec0d435c290a657b8a9a0adf6da0cfb6c4c3c20
Dhanush0777 <dhanushvulapelli@gmail.com>
2026-08-30 16:40:35 +0530

    Add src/main/java/com/medtime/repository/MedicineRepository.java

---
+++ src/main/java/com/medtime/repository/MedicineRepository.java
@@ -0,0 +1,12 @@
+package com.medtime.repository;
+
+import com.medtime.entity.Medicine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MedicineRepository extends JpaRepository<Medicine, Long> {
+    List<Medicine> findByPrescriptionId(Long prescriptionId);
+}
