7b1d17665d644765ec838f8434d1b94a88e6db68
Dhanush0777 <dhanushvulapelli@gmail.com>
2026-08-30 16:39:36 +0530

    Add src/main/java/com/medtime/MediTimeApplication.java

---
+++ src/main/java/com/medtime/MediTimeApplication.java
@@ -0,0 +1,12 @@
+package com.medtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediTimeApplication.class, args);
+    }
+}
